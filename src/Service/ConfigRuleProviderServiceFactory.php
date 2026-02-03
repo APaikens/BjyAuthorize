ec441810d04751bf5e40ba84d79c83e18768d3ef
Artūrs.Ozoliņš <arturs.ozolins@di.lv>
2026-02-03 17:15:36 +0200

    #50917 Missed this?

--- src/Service/ConfigRuleProviderServiceFactory.php
+++ src/Service/ConfigRuleProviderServiceFactory.php
@@ -17,7 +17,7 @@ class ConfigRuleProviderServiceFactory implements FactoryInterface
      * {@inheritDoc}
      * @see \Laminas\ServiceManager\Factory\FactoryInterface::__invoke()
      */
-    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
         return new Config(
             $container->get('BjyAuthorize\Config')['rule_providers'][Config::class]
